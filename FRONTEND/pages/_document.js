7003df0e5ad39f4b4da91c0beb54b2f4925a0df2
naxim-khan <nazeemkhanpk@gmail.com>
2024-11-22 19:04:12 +0500

    added google search console meta tag

--- FRONTEND/pages/_document.js
+++ FRONTEND/pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Russo+One&family=Sora:wght@100..800&display=swap" rel="stylesheet" />
-
+        <meta name="google-site-verification" content="nDvElDBuo4429FmAToBQ-P-5es5MuQt5E3L_epb755k" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
       </Head>
       <body>
